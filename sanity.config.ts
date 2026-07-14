1163ce7775dca6b4077f2e98f489349f1ccad409
PensivePasta <pensivepasta@gmail.com>
2026-07-14 17:16:10 +0100

    Update config

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@ import MyLogo from './components/MyLogo';
 
 export default defineConfig({
   name: 'default',
-  title: 'bba-cms',
+  title: 'Wearebba',
   projectId: '9s2ar5mb',
   dataset: 'production',
   icon: MyLogo,             
